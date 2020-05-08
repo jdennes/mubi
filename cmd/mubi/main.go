b7b4268abf94eed20d6e9664ff79175f35780205
James Dennes <jdennes@gmail.com>
2020-05-08 16:17:16 +0200

    Define GetRatings function

--- cmd/mubi/main.go
+++ cmd/mubi/main.go
@@ -1,46 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"time"
-
   "github.com/jdennes/mubi"
 )
 
 func main() {
-	userId := 7995037
-	url := fmt.Sprintf("https://mubi.com/services/api/ratings?user_id=%d", userId)
-
-	client := http.Client{
-		Timeout: time.Second * 2,
-	}
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header.Set("User-Agent", "jdennes/mubi")
-	res, getErr := client.Do(req)
-	if getErr != nil {
-		log.Fatal(getErr)
-	}
-	body, readErr := ioutil.ReadAll(res.Body)
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
-
-	ratings := make([]mubi.Rating, 0)
-	jsonErr := json.Unmarshal(body, &ratings)
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
-
+  // Just get a user's ratings as an example at this point
+  ratings := mubi.GetRatings(7995037)
 	for _, rating := range ratings {
 		fmt.Printf("%s: %s\n", rating.Film.Title, rating.Film.CanonicalUrl)
-		fmt.Println("Rating: ", rating.Overall)
+		fmt.Println("Rating:", rating.Overall)
 		fmt.Println("-----")
 	}
 }
--- ratings.go
+++ ratings.go
@@ -1,10 +1,44 @@
 package mubi
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
 type Rating struct {
 	Overall int `json:"overall"`
 	Film    Film
 }
 
-type Ratings struct {
-	Collection []Rating
+func GetRatings(userId int64) []Rating {
+	url := fmt.Sprintf("https://mubi.com/services/api/ratings?user_id=%d", userId)
+
+	client := http.Client{
+		Timeout: time.Second * 5,
+	}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("User-Agent", "jdennes/mubi")
+	res, getErr := client.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	ratings := make([]Rating, 0)
+	jsonErr := json.Unmarshal(body, &ratings)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	return ratings
 }
