a0699945dc9b411fee87db3f642c0bce8e5cc720
James Dennes <jdennes@gmail.com>
2020-08-29 16:06:50 +0200

    Add directors to film

--- film.go
+++ film.go
@@ -1,7 +1,13 @@
 package mubi
 
+type Director struct {
+	Id   int    `json:id`
+	Name string `json:name`
+}
+
 type Film struct {
-	Title        string `json:title`
-	Year         int    `json:year`
-	CanonicalUrl string `json:"canonical_url"`
+	Title        string     `json:title`
+	Year         int        `json:year`
+	CanonicalUrl string     `json:"canonical_url"`
+	Directors    []Director `json:directors`
 }
