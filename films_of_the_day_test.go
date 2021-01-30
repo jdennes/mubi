07cc3bd1dde38be03fd7d6cd4db02f8e6906cc90
James Dennes <jdennes@gmail.com>
2021-01-30 16:20:01 +0100

    Fix reference

--- films_of_the_day_test.go
+++ films_of_the_day_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestGetFilmsOfTheDay(t *testing.T) {
 	client := NewTestClient(func(req *http.Request) *http.Response {
-		file, _ := os.Open("testdata/mubi.com-film-of-the-day.html")
+		file, _ := os.Open("testdata/mubi-film-of-the-day-sample.html")
 		return &http.Response{
 			StatusCode: 200,
 			Body:       ioutil.NopCloser(file),
