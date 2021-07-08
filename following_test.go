43c1b2c00e7e394ad8fc72247c1a9e6556917f29
James Dennes <jdennes@gmail.com>
2021-07-08 17:47:13 +0200

    Use a better following fixture

--- following_test.go
+++ following_test.go
@@ -18,18 +18,18 @@ func TestGetFollowing(t *testing.T) {
 	})
 
 	api := MubiAPI{client}
-	userId, page, perPage := int64(7995037), 1, 20
+	userId, page, perPage := int64(6164674), 1, 20
 	following := api.GetFollowing(userId, page, perPage)
 
-	if len(following) != 2 {
-		t.Errorf("Number of following items was incorrect. Got: %d, expected: %d.", len(following), 2)
+	if len(following) != 20 {
+		t.Errorf("Number of following items was incorrect. Got: %d, expected: %d.", len(following), 20)
 	}
 
-	if following[0].Followee.Name != "Manuel" {
-		t.Errorf("Name of first followed user was incorrect. Got: %s, expected: %s.", following[0].Followee.Name, "Manuel")
+	if following[0].Followee.Name != "mubianer" {
+		t.Errorf("Name of first followed user was incorrect. Got: %s, expected: %s.", following[0].Followee.Name, "mubianer")
 	}
 
-	if following[1].Followee.Name != "josé neves" {
-		t.Errorf("Name of second followed user was incorrect. Got: %s, expected: %s.", following[1].Followee.Name, "josé neves")
+	if following[1].Followee.Name != "RogerTheMovieManiac88" {
+		t.Errorf("Name of second followed user was incorrect. Got: %s, expected: %s.", following[1].Followee.Name, "RogerTheMovieManiac88")
 	}
 }
